644835a046b055feb9ae5e4b6a9fe40e9965c887
TuanBT <bttvn.4t@gmail.com>
2026-03-08 15:40:13 +0700

    Delete File

--- dist/bin/gitkrapro-appid.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const program = require("commander");
-program
-    .name("gitkrapro-appid")
-    .description("GitKraPro AppId")
-    .command("generate", "generate GitKraPro AppId")
-    .command("read", "read GitKraPro AppId from config")
-    .parse(process.argv);
